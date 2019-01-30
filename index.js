8edb87ce814c2861c9d42a07136474ea8c626c7b
aberm <abermendel@gmail.com>
2019-01-30 12:15:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array){
+  return array.map(function(item){
+    return item.toLowerCase();
+  })
+}
+
+function nameToAttributes(array){
+  return array.map(function(item){
+    return Object.assign({}, { firstName: `${item.split(" ")[0]}`, lastName: `${item.split(" ")[1]}` });
+  })
+}
+
+function attributesToPhrase(array){
+  return array.map(function(item){
+    return `${item.name} is from ${item.hometown}`;
+  })
+}
